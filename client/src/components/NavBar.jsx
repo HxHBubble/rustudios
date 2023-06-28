b5fefb0b79af37b138b47f59e44674806ea3517b
Bubbles <hxhbubble@gmail.com>
2023-06-29 01:51:29 +0530

    Navbar Undo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100vw', height: '10vh', backgroundColor: 'transparent' }}>
+    <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100vw', height: '10vh', backgroundColor: 'transparent', display:'flex' }}>
       <Typography variant="h4" sx={{
         fontSize: '1.8rem', fontVariant: 'petite-caps', letterSpacing: 3, margin: '20px', width: 'fit-content', textShadow: '0 0 1px #03e9f4, 0 0 1px #03e9f4, 0 0 5px #03e9f4, 0 0 5px #03e9f4', transition: 'all .5s',
         '&:hover': { textShadow: '0 0 15px #03e9f4, 0 0 35px #03e9f4, 0 0 60px #03e9f4, 0 0 110px #03e9f4', cursor: 'pointer' }
       }}>RU-Studio</Typography>
+
+<Typography variant="h4" sx={{textAlign:'center', margin:'auto', color:'bisque', '&:hover':{color:'aqua'}}}>About</Typography>
+
     </Box>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,15 @@ import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme'
 import { CssBaseline } from '@mui/material';
-import { Canvas } from '@react-three/fiber'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Canvas>
+  
         <App />
-      </Canvas>
+ 
     </ThemeProvider>
   </React.StrictMode>
 );
