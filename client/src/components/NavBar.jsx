f6e6eac5609ecb16b324241bee683ce8a4e218cc
mahesh-Krish <host1.mahesh@gmail.com>
2023-07-06 23:43:21 +0530

    font
    changed the font

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,9 +5,9 @@ const NavBar = () => {
   return (
     <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100vw', height: '10vh', backgroundColor: 'transparent', display:'flex' }}>
       <Typography variant="h4" sx={{
-        fontSize: '1.8rem', fontVariant: 'petite-caps', letterSpacing: 3, margin: '20px', width: 'fit-content', textShadow: '0 0 1px #03e9f4, 0 0 1px #03e9f4, 0 0 5px #03e9f4, 0 0 5px #03e9f4', transition: 'all .5s',
+        fontSize: '1.8rem', fontVariant: 'petite-caps', letterSpacing: 3.5, margin: '40px', width: 'fit-content', textShadow: '0 0 1px #03e9f4, 0 0 1px #03e9f4, 0 0 5px #03e9f4, 0 0 5px #03e9f4', transition: 'all .5s',
         '&:hover': { textShadow: '0 0 15px #03e9f4, 0 0 35px #03e9f4, 0 0 60px #03e9f4, 0 0 110px #03e9f4', cursor: 'pointer' }
-      }}>RU-Studio</Typography>
+      }}>𝗥𝗨-𝗦𝗧𝗨𝗗𝗜𝗢</Typography>
 
 <Typography variant="h4" sx={{textAlign:'center', margin:'auto', color:'bisque', '&:hover':{color:'aqua'}}}>About</Typography>
 <Typography variant="h4" sx={{textAlign:'first', margin:'auto', color:'bisque', '&:hover':{color:'aqua'}}}>projects</Typography>
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
 
             <Box id='home-page' sx={{ width: '100vw', height: '100vh' }}>
-                <Typography variant="h1" sx={{ paddingTop: '100px' }}>3D - Models</Typography>
+                <Typography variant="h1" sx={{ paddingTop: '150px' }}>𝟛𝔻 - 𝕄𝕠𝕕𝕖𝕝𝕤</Typography>
                 <Typography variant="subtitle1" sx={{ textAlign: 'center', width: '250px', margin: 'auto' }}>Customize 3d- Models/Objects for Gaming, Product Design, Architectural Designs</Typography>
 
             </Box>
